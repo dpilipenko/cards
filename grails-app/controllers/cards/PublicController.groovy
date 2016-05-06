75ab4254cb6df72978af775eb349056a810d8602
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2016-05-06 13:18:47 -0400

    Public_WWW URL mappings

--- grails-app/controllers/cards/PublicController.groovy
+++ grails-app/controllers/cards/PublicController.groovy
@@ -3,4 +3,11 @@ package cards
 class PublicController {
 
     def index() { }
+
+    def listProfiles() { }
+    def getProfile() { }
+    def listDecksForProfile() { }
+    def getDeck() { }
+    def listCardsForDeck() { }
+    def getCard() { }
 }
--- grails-app/controllers/cards/UrlMappings.groovy
+++ grails-app/controllers/cards/UrlMappings.groovy
@@ -3,6 +3,13 @@ package cards
 class UrlMappings {
 
     static mappings = {
+        "/public/"(controller:"public", action: "listProfiles")
+        "/public/$profileID"(controller: "public", action: "getProfile")
+        "/public/$profileID/decks"(controller: "public", action: "listDecksForProfile")
+        "/public/$profileID/decks/$deckID"(controller: "public", action: "getDeck")
+        "/public/$profileID/decks/$deckID/cards"(controller: "public", action: "listCardsForDeck")
+        "/public/$profileID/decks/$deckID/cards/$cardID"(controller: "public", action: "getCard")
+
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
